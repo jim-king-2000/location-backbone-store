d1053185b254203f989c71621bef9dc7d8ca9ba3
Jim <jim_king_2000@163.com>
2019-06-28 22:54:50 +0800

    refactory

--- src/TrackPlayerStore.js
+++ src/TrackPlayerStore.js
@@ -3,7 +3,6 @@ import { observable } from 'mobx';
 export class TrackPlayerStore {
   constructor(tracks) {
     this.tracks = tracks.map(t => ({
-      name: t.name,
       thingId: t.thingId,
       tracks: t.splittedTrack.flat()
     }));
