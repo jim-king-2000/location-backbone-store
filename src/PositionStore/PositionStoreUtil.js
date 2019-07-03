e31ec5765b948d12ba9f15d904476b5db32f17f3
Jim <jim_king_2000@163.com>
2019-07-03 15:56:04 +0800

    refactory

---
+++ src/PositionStore/PositionStoreUtil.js
@@ -0,0 +1,49 @@
+import { TsdbClient } from 'location-backbone-sdk';
+import { appId, authorization } from './account';
+
+const tsdbClient = new TsdbClient();
+
+export function getEnabledThingIds(vehicles) {
+  const vehiclesEnabled = vehicles.filter(v => v.enabled);
+  return vehiclesEnabled.map(v => v.thingId);
+}
+
+export function onConnect(vehicles, socket) {
+  setTimeout(() => socket.send({ sub: getEnabledThingIds(vehicles) }), 1000);
+  console.log('Socket.io connected.');
+}
+
+export function onMessage(data, index, positions) {
+  console.log(data);
+  const i = index.get(data.thingId);
+  if (i === undefined) return;
+  data.data.forEach(d => {
+    positions[i] = {
+      ...positions[i],
+      ...d,
+      ...d.location,
+      ...d.sensors
+    };
+  });
+}
+
+export async function getPositions(vehicles) {
+  return Promise.all(
+    vehicles.map(async (v, i) => ({
+      ...v,
+      colorIndex: i,
+      ...await tsdbClient.getLastPosition({
+        appId,
+        thingId: v.thingId,
+        authorization })
+    }))
+  );
+}
+
+function isOnline(prevTimestamp) {
+  return Date.now() - prevTimestamp < 10000;
+}
+
+export function calcOnline(positions) {
+  positions.forEach(p => p.isOnline = isOnline(p.timestamp));
+}
--- src/PositionStore/index.js
+++ src/PositionStore/index.js
@@ -1,46 +1,7 @@
 import { observable, autorun } from 'mobx';
 import io from 'socket.io-client';
-import { TsdbClient } from 'location-backbone-sdk';
-import { appId, authorization } from './account';
-
-const tsdbClient = new TsdbClient();
-
-function getEnabledThingIds(vehicles) {
-  const vehiclesEnabled = vehicles.filter(v => v.enabled);
-  return vehiclesEnabled.map(v => v.thingId);
-}
-
-function onConnect(vehicles, socket) {
-  setTimeout(() => socket.send({ sub: getEnabledThingIds(vehicles) }), 1000);
-  console.log('Socket.io connected.');
-}
-
-function onMessage(data, index, positions) {
-  console.log(data);
-  const i = index.get(data.thingId);
-  if (i === undefined) return;
-  data.data.forEach(d => {
-    positions[i] = {
-      ...positions[i],
-      ...d,
-      ...d.location,
-      ...d.sensors
-    };
-  });
-}
-
-async function getPositions(vehicles) {
-  return Promise.all(
-    vehicles.map(async (v, i) => ({
-      ...v,
-      colorIndex: i,
-      ...await tsdbClient.getLastPosition({
-        appId,
-        thingId: v.thingId,
-        authorization })
-    }))
-  );
-}
+import { getEnabledThingIds, onConnect, onMessage,
+  getPositions, calcOnline } from './PositionStoreUtil';
 
 export class PositionStore {
   constructor(vehicles, url) {
@@ -50,6 +11,7 @@ export class PositionStore {
       const checkedVehicles = this.vehicles.filter(v => v.enabled);
       const positions = await getPositions(checkedVehicles);
       this.positionIndex = new Map(positions.map((p, i) => [p.thingId, i]));
+      calcOnline(positions);
       this.positions = positions;
     });
 
