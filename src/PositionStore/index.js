614a301e9f90fb40e4dd00eb1997c2850c91621c
Jim <jim_king_2000@163.com>
2019-07-05 11:35:29 +0800

    refactory

--- src/PositionStore/index.js
+++ src/PositionStore/index.js
@@ -12,6 +12,10 @@ export class PositionStore {
       const positions = await getPositions(checkedVehicles);
       this.positionIndex = new Map(positions.map((p, i) => [p.thingId, i]));
       calcOnline(positions);
+
+      // 以下两行位置不可以交换，否则数组proxy赋值会生成元素的拷贝，导致
+      // selectedVehicle和this.positions[i]指向不同的对象。这会使得单车窗口
+      // 停止跟随车辆移动。
       this.positions = positions;
       this.selectedVehicle = refreshSelectedVehicle(
         this.selectedVehicle,
