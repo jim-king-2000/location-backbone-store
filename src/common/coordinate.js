4881b5565d50705997811e62c2cca6ebef1cdb1a
Jim <jim_king_2000@163.com>
2019-09-17 14:21:38 +0800

    refactory

--- src/common/coordinate.js
+++ src/common/coordinate.js
@@ -22,7 +22,6 @@ function transformPoint(p, sourceCoordinateType, targetCoordinateType) {
 
 export function coordinateTransform(p, targetCoordinateType = 'gcj-02') {
   const coordinateType = p.coordinateType || 'gcj-02';
-  console.log(coordinateType, targetCoordinateType)
-  if (!TypeMap.get(coordinateType)) return p;
+  if (!TypeMap.get(coordinateType)) coordinateType = 'gcj-02';
   return transformPoint(p, coordinateType, targetCoordinateType);
 }
