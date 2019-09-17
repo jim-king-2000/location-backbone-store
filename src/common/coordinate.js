e99fdb5e1f22f8b708578bd2deeab9f5da30796b
Jim <jim_king_2000@163.com>
2019-09-17 13:32:55 +0800

    refactory coordinateTransform

--- src/common/coordinate.js
+++ src/common/coordinate.js
@@ -1,20 +1,20 @@
 import { transform, WGS84, GCJ02, BD09 } from 'gcoord';
 
-function transformPoint(p, sourceCoordinateType) {
+function transformPoint(p, sourceCoordinateType, targetCoordinateType = GCJ02) {
   const [longitude, latitude] = transform(
     [p.longitude, p.latitude],
     sourceCoordinateType,
-    GCJ02
+    targetCoordinateType
   );
   p.longitude = longitude;
   p.latitude = latitude;
 }
 
-export function coordinateTransform(p) {
+export function coordinateTransform(p, targetCoordinateType = GCJ02) {
   if (p.coordinateType === 'wgs-84') {
-    transformPoint(p, WGS84);
+    transformPoint(p, WGS84, targetCoordinateType);
   } else if (p.coordinateType === 'bd-09') {
-    transformPoint(p, BD09)
+    transformPoint(p, BD09, targetCoordinateType)
   }
 
   return p;
